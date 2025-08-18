0fd3e2009308a0a749daa66ef17397c91a122c14
domespa <dumiii1988@gmail.com>
2025-08-18 16:08:57 +0200

    test connection supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 // SUPABASE
 import { supabase } from "./utils/supabase";
 
+// HOOK
+import { useEffect } from "react";
+
 export default function App() {
+  useEffect(() => {
+    fetchSupa();
+  }, []);
+
+  const fetchSupa = async () => {
+    try {
+      const { data, error } = await supabase
+        .from(`travel_notes`)
+        .select(`*`)
+        .order(`created_at`, { ascending: false });
+
+      if (error) throw error;
+      console.log("💚Connessione a supabase riuscita!");
+    } catch (error) {
+      console.error("Connessione a supabase non riuscita:", error.message);
+    }
+  };
+
   return (
     <>
       <h1>Progetto con supabase</h1>
